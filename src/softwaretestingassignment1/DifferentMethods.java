f7200efa8871e5521aec6b512365dccea70abe70
adham89 <a_elahmad@hotmail.se>
2015-11-18 01:18:06 +0100

    Merge origin/master

---
+++ src/softwaretestingassignment1/DifferentMethods.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwaretestingassignment1;
+
+/**
+ *
+ * @author ad
+ */
+public class DifferentMethods {
+    
+    
+    public int add(int a, int b){
+        return 0;
+    }
+}
---
+++ test/TestDifferentMethods.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import softwaretestingassignment1.DifferentMethods;
+
+/**
+ *
+ * @author ad
+ */
+public class TestDifferentMethods {
+    
+    public TestDifferentMethods() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+        @Test
+    public void testAddition() {
+        System.out.println("addition");
+        int a = 100;
+        int b = 100;
+            DifferentMethods instance = new DifferentMethods();
+        int expResult = 200;
+        int result = instance.add(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+
+}
