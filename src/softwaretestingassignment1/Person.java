5a5975d51297c7872abdd7238ca42f7760daf41e
adham89 <a_elahmad@hotmail.se>
2015-11-26 18:45:31 +0100

    Creation of Person Class

---
+++ src/softwaretestingassignment1/Person.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwaretestingassignment1;
+
+/**
+ *
+ * @author ad
+ */
+public class Person {
+    
+    public int age;
+  
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return 0;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge() {
+        
+    }
+
+
+    
+}
